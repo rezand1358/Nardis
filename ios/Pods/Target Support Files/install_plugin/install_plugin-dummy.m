5bb0dbebc6ca2066878679ab9fec1147ff6e0b1e
rezand1358 <rndbusines@gmail.com>
2019-11-02 22:06:27 -0700

    tt
    tt

---
+++ ios/Pods/Target Support Files/install_plugin/install_plugin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_install_plugin : NSObject
+@end
+@implementation PodsDummy_install_plugin
+@end
---
+++ ios/PodsFile.swift
@@ -0,0 +1,9 @@
+//
+//  PodsFile.swift
+//  Runner
+//
+//  Created by Reza on 10/26/19.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
